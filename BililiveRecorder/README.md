850de8fffef5cb2707b33254ca08f2dffc35faf4
Jay-Young <8641055+Jay-Young@users.noreply.github.com>
2023-09-27 19:42:52 +0800

    publish: 🎉B站录播姬 v2.9.0

---
+++ BililiveRecorder/README.md
@@ -0,0 +1,16 @@
+# B 站录播姬
+
+源项目: <https://github.com/BililiveRecorder/BililiveRecorder>
+
+一个方便好用免费开源的 mikufans 直播录制工具。
+
+![BililiveRecorder](./BililiveRecorder.png)
+
+- 理论上会自动新建工作目录，默认为共享文件夹 `Download` 下新建的 `BililiveRecorder` 文件夹。如果安装失败，请手动创建工作目录。
+- 默认端口 `8923`，用户名 `qnap`，密码取 `$HOSTNAME` 的值，即控制台常规设置里的服务器名称，如果本机默认位置 `/etc/stunnel/` 存在证书，则以 `HTTPS` 运行，否则以 `HTTP` 运行。
+
+[v2.9.0](https://github.com/Jay-Young/qpkg/releases/tag/v2.9.0_BililiveRecorder)
+
+- MD5:
+  - arm_64: 05263281a0d55a258f378a2ef7eac496
+  - x86_64: 36598c9c859df0cdbca56c7a28be8de1
---
+++ BililiveRecorder/shared/BililiveRecorder.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+CONF=/etc/config/qpkg.conf
+QPKG_NAME="BililiveRecorder"
+QPKG_ROOT=`/sbin/getcfg $QPKG_NAME Install_Path -f ${CONF}`
+APACHE_ROOT=`/sbin/getcfg SHARE_DEF defWeb -d Qweb -f /etc/config/def_share.info`
+export QNAP_QPKG=$QPKG_NAME
+
+VolMP_ROOT=$(/sbin/getcfg SHARE_DEF defVolMP -f /etc/config/def_share.info)
+if [ -d "$VolMP_ROOT/Download/BililiveRecorder" ]; then
+  workdir="$VolMP_ROOT/Download/BililiveRecorder"
+else
+  /sbin/log_tool -t2 -a"[B站录播姬] 未找到工作目录，请手动创建。"
+  exit 1
+fi
+
+case "$1" in
+  start)
+    ENABLED=$(/sbin/getcfg $QPKG_NAME Enable -u -d FALSE -f $CONF)
+    if [ "$ENABLED" != "TRUE" ]; then
+        echo "$QPKG_NAME is disabled."
+        exit 1
+    fi
+    cd "$QPKG_ROOT/bin"
+    if [ -f "/etc/stunnel/stunnel.pem" ]; then
+      ./BililiveRecorder.Cli run --bind "https://*:8923" --http-basic-user "qnap" --http-basic-pass "$HOSTNAME" --cert-pem-path "/etc/stunnel/stunnel.pem" --cert-key-path "/etc/stunnel/stunnel.pem" "$workdir" &
+    else
+      ./BililiveRecorder.Cli run --bind "http://*:8923" --http-basic-user "qnap" --http-basic-pass "$HOSTNAME" "$workdir" &
+    fi
+    ;;
+
+  stop)
+    pid=$(lsof -i:8923 | awk '{print $2}' | grep -v PID)
+    if [ -n "$pid" ]; then
+      kill "$pid"
+    fi
+    ;;
+
+  restart)
+    $0 stop
+    $0 start
+    ;;
+  remove)
+    ;;
+
+  *)
+    echo "Usage: $0 {start|stop|restart|remove}"
+    exit 1
+esac
+
+exit 0
